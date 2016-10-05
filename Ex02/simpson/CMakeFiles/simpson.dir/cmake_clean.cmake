3d57cb0234a4c816667fbcf9da4e5d7f8cab6e32
blaunet <blaunet@student.ethz.ch>
2016-10-05 19:22:23 +0200

    Trying to implement libraries with little success

---
+++ Ex02/simpson/CMakeFiles/simpson.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpson.dir/simpson_main.cpp.o"
+  "CMakeFiles/simpson.dir/simpson_method.cpp.o"
+  "simpson.pdb"
+  "simpson"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
