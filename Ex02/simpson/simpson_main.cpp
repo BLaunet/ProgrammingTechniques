376eac8e75b15e066237fb4bc180b6bcef941be9
blaunet <blaunet@student.ethz.ch>
2016-10-05 18:34:35 +0200

    Reorganiszing Folders

---
+++ Ex02/simpson/simpson_main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include "simpson_method.hpp"
+#include "simpson_method.cpp"
+
+using namespace std;
+
+int main()
+{
+    int N = 100;
+    double a = 0;
+    double b = M_PI;
+    cout.precision(15);
+    cout << integrate(a, b, N, std::sin) << endl;
+    return 0;
+}
+
