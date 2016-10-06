26bc8f6974caee77e58c1c7b38888e8185ee3688
blaunet <blaunet@student.ethz.ch>
2016-10-06 15:50:29 +0200

    Ex2 is now clean

---
+++ Ex02/build/CMakeFiles/simpson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpson.dir/Users/Barth/Documents/ETH_Zurich/Programming_Techniques/Excercises/Ex02/src/simpson.cpp.o"
+  "libsimpson.pdb"
+  "libsimpson.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
