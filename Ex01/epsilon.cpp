14956aec34fb56666ee3f8ceb551a7080f301188
blaunet <blaunet@student.ethz.ch>
2016-10-06 15:04:38 +0200

    Correcting epslion.cpp

--- Ex01/epsilon.cpp
+++ Ex01/epsilon.cpp
@@ -4,27 +4,12 @@
 using namespace std;
 int main()
 {
-    double max = 1;
-    double min = 0;
-    double mid = 0.5;
-    double newMid = mid;
-    double limit = 8e-15;
-    
-    cout.precision(50);
-    
-    while (true)
+    double eps = 1;
+    while (eps/2 + 1 != 1)
     {
-        if(mid+1 == 1)
-            min = mid;
-        else
-            max = mid;
-        
-        newMid = (max+min)/2;
-        if(abs(max - min) < limit)
-            break;
-        mid = newMid;
+        eps/=2;
     }
-    cout << max/2 << endl;
-    cout << std::numeric_limits<double>::epsilon();
+    cout << "Result from our function : " << eps << endl;
+    cout << "Result from the limits function : " << std::numeric_limits<double>::epsilon() << endl;
     return 0 ;
 }
